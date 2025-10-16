1364d016c4211c7bd46f79c1cbcf6b53473831b3
Jakob Zanker <jakob@project-lary.com>
2025-10-16 17:14:55 +0200

    Add support for Flask-OpenApi3

--- src/pytest_api_cov/frameworks.py
+++ src/pytest_api_cov/frameworks.py
@@ -95,7 +95,9 @@ def get_framework_adapter(app: Any) -> BaseAdapter:
 
     if module_name == "flask" and app_type == "Flask":
         return FlaskAdapter(app)
-    if module_name == "fastapi" and app_type == "FastAPI":
+    elif module_name == "flask_openapi3" and app_type == "OpenAPI":
+        return FlaskAdapter(app)
+    elif module_name == "fastapi" and app_type == "FastAPI":
         return FastAPIAdapter(app)
 
     raise TypeError(f"Unsupported application type: {app_type}. pytest-api-coverage supports Flask and FastAPI.")
